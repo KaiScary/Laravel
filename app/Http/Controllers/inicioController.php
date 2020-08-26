fa98fc2f1ec45b4e83e724c93b31776476ae8cab
juansg3701 <54075068+juansg3701@users.noreply.github.com>
2020-08-26 17:00:25 -0500

    Update inicioController.php

--- app/Http/Controllers/inicioController.php
+++ app/Http/Controllers/inicioController.php
@@ -4,22 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+//inicializacion del array
+$items=array();
 class inicioController extends Controller
 {
+        
     public function index(){
     	$numero=2334;
-    	return view('welcome',["numero"=>$numero]);
+    	return view('welcome',["numero"=>$numero, "items"=>$items]);
     }
 
     public function store(Request $request){
+        //contraseña y correo que vienen de la vista welcome
     	$correo=$request->get('correo');
     	$contraseña=$request->get('contraseña');
 
     	$numero=24213;
+
+        //arrays para gusrdar contraseña y correo
+         $items=array("correo"=>$correo,"contraseña"=>$contraseña);
+         //array de objetos
+          $versiones = array("productos" => array($items));
+          //cambia array a json
+          json_encode($items);   
     	
-    	return view('welcome',["correo"=>$correo,"contraseña"=>$contraseña,"numero"=>$numero]);	
+        //envío de parametros a la vista welcome
+    	return view('welcome',["correo"=>$correo,"contraseña"=>$contraseña,"numero"=>$numero,"items"=>$items,"versiones"=>$versiones]);	
     }
 
-    public function
+    
 }
