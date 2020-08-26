e7dcb7116ed99c1fdb1a57f885c5f89c14dc0884
juansg3701 <54075068+juansg3701@users.noreply.github.com>
2020-08-26 12:39:52 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Route::post('/inicio', 'inicioController@store');
+//Route::get('/inicio', 'inicioController@index');
+
+Route::resource('/inicio', 'inicioController');
